7c5a04f40faa9858ec2b6c69459ffff2ab1ee2aa
RomeoSaveMe69 <romeosaveme6969@gmail.com>
2026-02-25 18:41:21 +0630

    Force deploy login page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  trailingSlash: false, // /login only (not /login/)
 };
 
 export default nextConfig;
